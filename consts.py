e59c6a6a0f2e4a9070d872b631c1a21948f7a882
mytja <mytja@protonmail.com>
2022-12-03 20:21:23 +0100

    testi

--- consts.py
+++ consts.py
@@ -1,5 +1,71 @@
 import os
 
+from sqlalchemy import Column, String, Boolean, Integer
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+
 MS_OAUTH_ID = os.environ["MS_OAUTH_ID"]
 MS_OAUTH_SECRET = os.environ["MS_OAUTH_SECRET"]
 SCOPE = "https://graph.microsoft.com/Files.Read.All"
+
+ALLOWED_EXTENSIONS = [
+    "pdf",  # Portable Document Format
+    "pptx",  # PowerPoint format
+    "ppt",  # Old PowerPoint document format
+    "md",  # Markdown & Mayura Draw (*cough* pls no)
+    "doc",
+    "docx",
+    "xls",
+    "xlsx",
+    "txt",
+    "png",
+    "jpg",
+    "jpeg",
+    "bmp",
+    "gif",
+    "svg",
+    "webm",  # Only movie format allowed, as it's very small
+    "odt",  # LibreOffice Writer
+    "ods",  # LibreOffice Calc
+    "odp",  # LibreOffice Impress
+    "odg",  # LibreOffice Draw
+    "odf",  # LibreOffice Math
+]
+
+DATABASE_USER = os.environ["POSTGRES_USER"]
+DATABASE_PASSWORD = os.environ["POSTGRES_PASSWORD"]
+DATABASE_CONNECTION = f"postgresql+asyncpg://{DATABASE_USER}:{DATABASE_PASSWORD}@127.0.0.1:5432/BeziAppDB"
+
+engine = create_async_engine(DATABASE_CONNECTION)
+async_session = sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession
+)
+
+Base = declarative_base()
+
+
+class Upload(Base):
+    __tablename__ = 'upload'
+    id = Column(String(60), primary_key=True)
+    filename = Column(String(100), unique=True)
+    username = Column(String(60))
+    filepath = Column(String(150))
+    subject = Column(String(60))
+    teacher = Column(String(60))
+    class_name = Column(String(60))
+    class_year = Column(String(12))
+    pending_moderation = Column(Boolean)
+    type = Column(String(30))
+
+
+class UploadJSON:
+    def __init__(self, id: str, filename: str, subject: str, teacher: str, class_name: str, class_year: str, type: str,
+                 uploaded_by_me: bool):
+        self.id = id
+        self.filename = filename
+        self.subject = subject
+        self.teacher = teacher
+        self.class_name = class_name
+        self.class_year = class_year
+        self.type = type
+        self.uploaded_by_me = uploaded_by_me
--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import json
 import os
 import re
 import io
+import uuid
 
 import aiofiles as aiofiles
 import httpx
-from fastapi import FastAPI, status, Header, Response, Form
-from fastapi.responses import RedirectResponse, StreamingResponse
+from fastapi import FastAPI, status, Header, Response, Form, UploadFile
+from fastapi.responses import RedirectResponse, StreamingResponse, FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 from ics import Calendar, Event
 from datetime import datetime
@@ -19,11 +20,12 @@ from gimsisapi import GimSisAPI
 from lopolis import LoPolisAPI
 
 import tabula
+from sqlalchemy import select, delete
 
-from consts import MS_OAUTH_ID, SCOPE, MS_OAUTH_SECRET
+from consts import MS_OAUTH_ID, SCOPE, MS_OAUTH_SECRET, engine, Base, Upload, async_session, UploadJSON, \
+    ALLOWED_EXTENSIONS
 
 app = FastAPI()
-
 
 origins = [
     "*",
@@ -367,6 +369,105 @@ async def set_meals(response: Response, month: str, year: str, authorization: st
     return await lopolis_session.set_checkouts(year, month, json.loads(lopolis_response))
 
 
+# BežiTesti
+@app.post("/notes/upload", status_code=status.HTTP_201_CREATED)
+async def upload_new_note(
+        response: Response,
+        file: UploadFile,
+        subject: str = Form(),
+        teacher: str = Form(),
+        class_name: str = Form(),
+        class_year: str = Form(),
+        type: str = Form(),
+        authorization: str = Header(),
+):
+    if authorization == "" or sessions.get(authorization) is None:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return
+    gimsis_session = sessions[authorization]
+    id = str(uuid.uuid4())
+    extension = file.filename.split(".")[-1]
+    if extension not in ALLOWED_EXTENSIONS:
+        return "Extension isn't allowed. Please contact system administrators if you think it's a mistake."
+    file_path = f"uploads/{id}.{extension}"
+    contents = await file.read()
+    async with aiofiles.open(file_path, "wb+") as f:
+        await f.write(contents)
+    upload = Upload(
+        id=id,
+        filename=file.filename,
+        username=gimsis_session.username,
+        filepath=file_path,
+        subject=subject,
+        teacher=teacher,
+        class_name=class_name,
+        class_year=class_year,
+        pending_moderation=False,
+        type=type,
+    )
+    async with async_session() as session:
+        session.add(upload)
+        await session.commit()
+
+
+@app.get("/notes", status_code=status.HTTP_200_OK)
+async def get_notes(response: Response, authorization: str = Header()):
+    if authorization == "" or sessions.get(authorization) is None:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return
+    gimsis_session = sessions[authorization]
+    async with async_session() as session:
+        uploads = (await session.execute(select(Upload))).all()
+    uploads_json = []
+    for i in uploads:
+        i = i[0]
+        uploads_json.append(UploadJSON(i.id, i.filename, i.subject, i.teacher,  i.class_name, i.class_year, i.type, i.username == gimsis_session.username))
+    return uploads_json
+
+
+@app.delete("/notes", status_code=status.HTTP_200_OK)
+async def get_notes(response: Response, id: str = Form(), authorization: str = Header()):
+    if authorization == "" or sessions.get(authorization) is None:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return
+    gimsis_session = sessions[authorization]
+    async with async_session() as session:
+        upload = (await session.execute(select(Upload).filter_by(id=id))).first()
+        if upload is None or upload[0] is None:
+            response.status_code = status.HTTP_404_NOT_FOUND
+            return
+
+        upload = upload[0]
+
+        if upload.username != gimsis_session.username:
+            response.status_code = status.HTTP_403_FORBIDDEN
+            return
+        try:
+            os.remove(upload.filepath)
+        except:
+            pass
+
+        await session.execute(delete(Upload).where(Upload.id == id))
+        await session.commit()
+
+
+@app.get("/notes/get", status_code=status.HTTP_200_OK)
+async def get_notes(response: Response, id: str, authorization: str = Header()):
+    if authorization == "" or sessions.get(authorization) is None:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return
+    gimsis_session = sessions[authorization]
+    async with async_session() as session:
+        upload = (await session.execute(select(Upload).filter_by(id=id))).first()
+        if upload is None or upload[0] is None:
+            response.status_code = status.HTTP_404_NOT_FOUND
+            return
+        upload = upload[0]
+        return FileResponse(upload.filepath)
+
+
 @app.on_event("startup")
 async def startup_event():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
     asyncio.ensure_future(background_sharepoint_job())
